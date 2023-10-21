96bb8b873d953e6f46d1048c07457303997256a7
iGxnon <igxnon@gmail.com>
2023-10-21 14:02:09 +0800

    perf: use bit vec queue in dedup layer

--- src/codec/dedup.rs
+++ src/codec/dedup.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, VecDeque};
+use std::fmt::Debug;
 use std::net::SocketAddr;
 use std::pin::Pin;
 use std::task::{ready, Context, Poll};
@@ -12,17 +13,119 @@ use crate::errors::CodecError;
 use crate::packet::connected::Uint24le;
 use crate::packet::{connected, PackId, Packet};
 
+const USIZE_BITS: usize = std::mem::size_of::<usize>() * 8;
+const DEFAULT_BIT_VEC_CAP: usize = 256 * USIZE_BITS;
+
+/// A one-direction bit vector queue
+/// It use a ring buffer `VecDeque<usize>` to store bits
+///
+/// Memory Layout:
+///
+/// `010000000101_110111011111_000001000000000`
+///  ^        ^                ^    ^
+///  |        |________________|____|
+///  |________|         len    |____|
+///     head                    tail
+#[derive(Debug, Default)]
+struct BitVecQueue {
+    store: VecDeque<usize>,
+    head: usize,
+    tail: usize,
+}
+
+impl BitVecQueue {
+    fn with_capacity(cap_bits: usize) -> Self {
+        Self {
+            store: VecDeque::with_capacity(cap_bits / USIZE_BITS),
+            head: 0,
+            tail: 0,
+        }
+    }
+
+    fn len(&self) -> usize {
+        if self.store.is_empty() {
+            return 0;
+        }
+        (self.store.len() - 1) * USIZE_BITS + self.tail - self.head + 1
+    }
+
+    fn get(&self, idx: usize) -> Option<bool> {
+        let idx = self.head + idx;
+        let index = idx / USIZE_BITS;
+        let slot = idx % USIZE_BITS;
+        let bits = self.store.get(index)?;
+        Some(bits & (1 << (USIZE_BITS - 1 - slot)) != 0)
+    }
+
+    fn set(&mut self, idx: usize, v: bool) {
+        let idx = self.head + idx;
+        let index = idx / USIZE_BITS;
+        let slot = idx % USIZE_BITS;
+        let Some(bits) = self.store.get_mut(index) else {
+            return;
+        };
+        if v {
+            *bits |= 1 << (USIZE_BITS - 1 - slot);
+        } else {
+            *bits &= !(1 << (USIZE_BITS - 1 - slot));
+        }
+    }
+
+    fn push(&mut self, v: bool) {
+        if self.tail == (USIZE_BITS - 1) || self.store.is_empty() {
+            self.tail = 0;
+            if v {
+                self.store.push_back(1 << (USIZE_BITS - 1));
+            } else {
+                self.store.push_back(0);
+            }
+            return;
+        }
+        self.tail += 1;
+        if v {
+            let last = self.store.back_mut().unwrap();
+            *last |= 1 << (USIZE_BITS - 1 - self.tail);
+        }
+    }
+
+    fn front(&self) -> Option<bool> {
+        let front = self.store.front()?;
+        Some(front & (1 << (USIZE_BITS - 1 - self.head)) != 0)
+    }
+
+    fn pop(&mut self) {
+        let len = self.store.len();
+        if self.head == (USIZE_BITS - 1) || len == 0 {
+            self.head = 0;
+            self.store.pop_front();
+            return;
+        }
+        if len == 1 && self.head == self.tail {
+            self.head = 0;
+            self.tail = 0;
+            self.store.clear();
+        }
+        self.head += 1;
+    }
+}
+
 #[derive(Debug, Default)]
 struct DuplicateWindow {
     /// First unreceived sequence number, start at 0
     first_unreceived: u32,
-    // TODO: use bit vec
     /// Record the received status of sequence numbers start at `first_unreceived`
     /// `true` is received and `false` is unreceived
-    received_status: VecDeque<bool>,
+    received_status: BitVecQueue,
 }
 
 impl DuplicateWindow {
+    fn with_capacity(cap_bits: usize) -> Self {
+        Self {
+            first_unreceived: 0,
+            received_status: BitVecQueue::with_capacity(cap_bits),
+        }
+    }
+
     /// Check whether a sequence number is duplicated
     fn duplicate(&mut self, seq_num: Uint24le) -> bool {
         if seq_num.0 < self.first_unreceived {
@@ -34,25 +137,25 @@ impl DuplicateWindow {
             // advance the first_unreceived and pop the front of
             // received_status
             self.first_unreceived += 1;
-            self.received_status.pop_front();
+            self.received_status.pop();
         } else if gap < self.received_status.len() {
             // received the sequence number that is recorded in received_status
             // check its status to determine whether it is duplicated
-            if self.received_status[gap] {
+            if self.received_status.get(gap) == Some(true) {
                 return true;
             }
             // mark it is received
-            self.received_status[gap] = true;
+            self.received_status.set(gap, true);
         } else {
             // received the sequence number that exceed received_status, extend
             // the received_status and record the received_status[gap] as received
-            let count = gap - self.received_status.len();
-            self.received_status
-                .extend(std::iter::repeat(false).take(count));
-            self.received_status.push_back(true);
+            for _ in 0..gap - self.received_status.len() {
+                self.received_status.push(false);
+            }
+            self.received_status.push(true);
         }
         while let Some(true) = self.received_status.front() {
-            self.received_status.pop_front();
+            self.received_status.pop();
             self.first_unreceived += 1;
         }
         false
@@ -101,7 +204,10 @@ where
         let Packet::Connected(connected::Packet::FrameSet(mut frame_set)) = packet else {
             return Poll::Ready(Some(Ok((packet, addr))));
         };
-        let window = this.windows.entry(addr).or_default();
+        let window = this
+            .windows
+            .entry(addr)
+            .or_insert_with(|| DuplicateWindow::with_capacity(DEFAULT_BIT_VEC_CAP));
         frame_set.frames.retain(|frame| {
             let Some(reliable_frame_index) = frame.reliable_frame_index else {
                 return true;
